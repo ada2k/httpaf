9de4d9a878b2bb19f31a660054bd9910fa1b5137
ada0 <ada0@noreply.codeberg.org>
2024-07-09 23:22:52 +0100

    working fallible flush

--- examples/eio/eio_echo_post.ml
+++ examples/eio/eio_echo_post.ml
@@ -1,6 +1,14 @@
 let () = Eio_main.run @@ fun env -> Eio.Switch.run @@ fun sw ->
   let request_handler _ reqd =
-    Httpaf.Reqd.respond_with_string reqd (Httpaf.Response.create ~headers:(Httpun_types.Headers.of_list ["connection", "close"]) `OK) "Hello World!"
+    let writer = Httpaf.Reqd.respond_with_streaming reqd (Httpaf.Response.create ~headers:(Httpun_types.Headers.of_list ["connection", "close"]) `OK) in
+    let rec push () =
+      Httpaf.Body.Writer.write_string writer "Chunk";
+      Httpaf.Body.Writer.flush writer (
+        function
+        | `Written -> push ()
+        | `Closed -> print_endline "Closed"; exit 0
+      )
+    in push ()
   in
   let handler = Httpaf_eio.Server.create_connection_handler ~error_handler:(fun _ ?request:_ error _ ->
     match error with
